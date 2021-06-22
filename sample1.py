2844e844cd81ec226e67d726b1b7b715d7720d1a
Takafumi <s19C1087@s.chibakoudai.jp>
2021-06-22 12:56:16 +0900

    add a file

---
+++ sample1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+digits = datasets.load_digits()
+print(digits.keys())
+print(digits.target[10])
+plt.imshow(digits.images[10], cmap = plt.cm.gray_r, interpolation = 'nearest')
+plt.savefig('./output_digit.jpg')
